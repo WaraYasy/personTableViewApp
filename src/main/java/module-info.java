43c263960085a43e47deb0eec00a459b0c371182
Warita <warayasyp@gmail.com>
2025-09-25 20:27:47 +0200

    CHORE: configurar Logback para logging.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module es.wara {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires org.slf4j;
 
     opens es.wara to javafx.fxml;
+    opens es.wara.control to javafx.fxml;
+    opens es.wara.model to javafx.base;
     exports es.wara;
+    exports es.wara.control;
+    exports es.wara.model;
 }

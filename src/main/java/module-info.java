f191d8132312a9e8dae95764b5c2fbb03455d0d2
Warita <warayasyp@gmail.com>
2025-09-27 12:50:08 +0200

    Cambios en la estructura dle proyecto. - Eliminada carpeta Util y PersonTableUtil.Java - Creada Carpeta de Logs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,26 @@
+/**
+ * Módulo de aplicación JavaFX para gestión de personas en tabla.
+ * 
+ * @author Wara Pacheco
+ * @version 1.1
+ */
 module es.wara {
+    // Dependencias JavaFX
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    
+    // Logging
     requires org.slf4j;
+    requires javafx.base;
 
+
+    // Permitir acceso por reflexión para JavaFX
     opens es.wara to javafx.fxml;
     opens es.wara.control to javafx.fxml;
     opens es.wara.model to javafx.base;
+    
+    // Paquetes públicos
     exports es.wara;
     exports es.wara.control;
     exports es.wara.model;
